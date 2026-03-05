f22bf6158e3f698cabbad55455ce93322bb8ff9e
Tan <e6194865@cmaisonneuve.qc.ca>
2026-03-05 16:46:42 -0500

    bug

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/ProjetFE/' : '/',
-  optimizeDeps: {
-    exclude: ['@dimforge/rapier3d-compat']
+  ssr: {
+    noExternal: ['@dimforge/rapier3d-compat']
   }
 });
